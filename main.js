37fedefa240b01017392b5221c9afab70ff5af4b
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-05 23:09:01 +0200

    Inserite info mosse e animazione al click di Protezione

--- main.js
+++ main.js
@@ -1,16 +1,48 @@
-// lista variabili
-var fight = document.getElementById("fighta");
-var back = document.getElementById("back");
 
-// apparizione mosse
-fight.addEventListener("click",
-  function () {
-    document.getElementById("opzionilotta").style.opacity = "1";
+$(document).ready(function(){
+
+  // open/close battle menu
+  $("#fighta, .back").click(
+    function(){
+      $(".containermosse").toggle();
+    }
+  )
+
+  // moves info on mouseover
+  $(".mossa1").mouseover(
+    function () {
+      $(".type").text("Type: Water");
+      $(".dmg").text("Damage: 90");
+    }
+  )
+
+  $(".mossa2").mouseover(
+    function () {
+      $(".type").text("Type: Earth");
+      $(".dmg").text("Damage: 100");
+    }
+  )
+
+  $(".mossa3").mouseover(
+    function () {
+      $(".type").text("Type: Ice");
+      $(".dmg").text("Damage: 120");
+    }
+  )
+
+  $(".mossa4").mouseover(
+    function () {
+      $(".type").text("Type: Normal");
+      $(".dmg").text("No dmg taken");
+    }
+  )
+
+  // moves used
+  $(".mossa4").click(
+    function() {
+      $("#blastoise").hide();
+      $("#blastoiseshell").show();
   }
-);
-// sparizione mosse
-back.addEventListener("click",
-  function () {
-    document.getElementById("opzionilotta").style.opacity = "0";
-  }
-);
+  )
+
+});
